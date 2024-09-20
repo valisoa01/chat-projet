da61d75c15f3ee93b333c27490061447a7a52266
Yosvah <eddyjosva08@gmail.com>
2024-09-20 15:59:24 +0300

    ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,47 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import io from 'socket.io-client';
-
-const socket = io('http://localhost:3001');  // Connexion au backend
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/login";
 
 function App() {
-  const [message, setMessage] = useState('');
-  const [messages, setMessages] = useState([]);
-
-  useEffect(() => {
-    // Écouter les messages provenant du serveur
-    socket.on('chat message', (msg) => {
-      setMessages((prevMessages) => [...prevMessages, msg]);
-    });
-  }, []);
-
-  const sendMessage = (e) => {
-    e.preventDefault();
-    if (message) {
-      socket.emit('chat message', message);  // Envoyer le message au backend
-      setMessage('');
-    }
-  };
-
   return (
-    <div className="App">
-      <h1>Chat en temps réel</h1>
-      <h1>Hello Guys</h1>
-      <ul>
-        {messages.map((msg, idx) => (
-          <li key={idx}>{msg}</li>
-        ))}
-      </ul>
-      <form onSubmit={sendMessage}>
-        <input 
-          value={message} 
-          onChange={(e) => setMessage(e.target.value)} 
-          placeholder="Votre message..." 
-        />
-        <button type="submit">Envoyer</button>
-      </form>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="*" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
-  <h1>Hello world</h1>
 }
-
 export default App;
---
+++ frontend/src/pages/login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const login = () => {
+  return (
+    <div>
+      hello you
+    </div>
+  )
+}
+
+export default login
